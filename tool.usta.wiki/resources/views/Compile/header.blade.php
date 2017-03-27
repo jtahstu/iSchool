e3aaf73ca00d228bf4ec1bb29c01713e9a576aca
jtusta <root@jtahstu.com>
2017-03-27 16:20:45 +0800

    修改iTool头部

--- tool.usta.wiki/resources/views/Compile/header.blade.php
+++ tool.usta.wiki/resources/views/Compile/header.blade.php
@@ -8,7 +8,9 @@
 				<nav id="J_Nav">
 					<ul id="J_Menu">
 						<li class="nav-1" data-menu="sub_menu_1" data-case="one">
-							<h2>iSchool</h2>
+							<a href="http://www.usta.wiki" target="_blank">
+								<h2>iSchool</h2>
+							</a>
 						</li>
 						<li class="nav-2" data-menu="sub_menu_2" data-case="two">
 							<h2>在线工具</h2>
@@ -26,46 +28,6 @@
 	</div>
 	<div id="J_subMenus" class="sub-menus" style="top: 99px;" data-spm="201">
 		<div id="sub_menu_1" class="sub-menu">
-			<!--<dl class="first" style="margin-left: 95.5px;">
-				<dt>
-				弹性计算
-				</dt>
-				<dd>
-					<a data-ga="导航.产品服务.ECS" href="http://www.aliyun.com/product/ecs/">
-						云服务器ECS
-					</a>
-				</dd>
-				<dd>
-					<a data-ga="导航.产品服务.SLB" href="http://www.aliyun.com/product/slb/">
-						负载均衡SLB
-					</a>
-				</dd>
-			</dl>
-			<dl>
-				<dt>
-				数据存储•计算
-				</dt>
-				<dd>
-					<a data-ga="导航.产品服务.RDS" href="http://www.aliyun.com/product/rds/">
-						关系型数据库服务RDS
-					</a>
-				</dd>
-				<dd>
-					<a data-ga="导航.产品服务.OSS" href="http://www.aliyun.com/product/oss/">
-						开放存储服务OSS
-					</a>
-				</dd>
-				<dd>
-					<a data-ga="导航.产品服务.CDN" href="http://www.aliyun.com/product/cdn/">
-						内容分发网络CDN
-					</a>
-				</dd>
-				<dd>
-					<a data-ga="导航.产品服务.OTS" href="http://www.aliyun.com/product/ots/">
-						开放结构化数据服务OTS
-					</a>
-				</dd>
-			</dl>-->
 		</div>
 		<div id="sub_menu_2" class="sub-menu">
 			<dl class="first" style="">
